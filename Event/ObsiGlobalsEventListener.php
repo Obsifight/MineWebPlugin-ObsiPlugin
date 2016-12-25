8436ebccaf1d50a2e73950e0907c5af0e46cf0e7
Valentin <contact@eywek.fr>
2016-12-25 19:04:40 +0100

    Add table didYouKnow, add cache, display vars only on homepage

--- Event/ObsiGlobalsEventListener.php
+++ Event/ObsiGlobalsEventListener.php
@@ -17,54 +17,47 @@ class ObsiGlobalsEventListener implements CakeEventListener {
 
   public function setVars() {
 
-    $this->controller->loadModel('News');
-    $NewsList = $this->controller->News->find('all', array('conditions' => array('published' => 1), 'order' => 'id desc', 'limit' => 5));
-    $this->controller->set('NewsList', $NewsList);
+    if ($this->controller->params['controller'] == "pages" && $this->controller->params['action'] == "display") {
+      // registered_count
+      $cache = Cache::read('registered_count', 'data');
+      if (!$cache) {
+        $registered_count = $this->controller->User->find('count');
+        Cache::write('registered_count', $registered_count, 'data');
+      } else {
+        $registered_count = $cache;
+      }
+      $this->controller->set('registered_count', $registered_count);
 
-    $registered_count = $this->controller->User->find('count');
-    $this->controller->set('registered_count', $registered_count);
+      // max players
+      $cache = Cache::read('maxPlayers', 'data');
+      if (!$cache) {
+        $this->CountPlayersLog = ClassRegistry::init('Obsi.CountPlayersLog');
+        $findMaxplayers = $this->CountPlayersLog->find('first', array('order' => 'players_online DESC'));
+        $maxPlayers = (isset($findMaxplayers['CountPlayersLog']['players_online'])) ? $findMaxplayers['CountPlayersLog']['players_online'] : 0;
+        Cache::write('maxPlayers', $maxPlayers, 'data');
+      } else {
+        $maxPlayers = $cache;
+      }
+      $this->controller->set('maxPlayers', $maxPlayers);
+    }
 
-    $this->CountPlayersLog = ClassRegistry::init('Obsi.CountPlayersLog');
-    $findMaxplayers = $this->CountPlayersLog->find('first', array('order' => 'players_online DESC'));
-    $maxPlayers = (isset($findMaxplayers['CountPlayersLog']['players_online'])) ? $findMaxplayers['CountPlayersLog']['players_online'] : 0;
-    $this->controller->set('maxPlayers', $maxPlayers);
-
-    if($this->controller->params['controller'] == "shop" && $this->controller->params['plugin'] == "shop") {
+    if ($this->controller->params['controller'] == "shop" && $this->controller->params['plugin'] == "shop") {
       $this->controller->set('header', false);
     }
 
-    $this->controller->set('didYouKnow', array(
-      'ObsiFight a été fondé par Suertz en 2014.',
-      'Sur la boutique, certaines solutions sont plus avantageuses que d\'autres.',
-      'La question du Mercredi revient... chaque mercredi.',
-      'La v4 d\'ObsiFight était en 1.8.',
-      'La limite d\'AP (4 claims) ne porte pas sur les coins.',
-      'La TNT au Xénotium a fait crasher le serveur, lors de son premier test.',
-      'Le KingZombie est apparu pour la première fois lors de la v4.',
-      'Herobrine a été retiré d\'ObsiFight.',
-      'L\'Obsidian est deux fois plus durable que le Garnet.',
-      'L\'Obsidian est plus résistant que le Titanium, mais fait moins de dégâts.',
-      'Les échelles de fer sont apparues pour la première fois lors de la v3.',
-      'Le Sadian s\'appelait auparavant Grobsi.',
-      'L\'Orbe de réparation peut être enchantée "Incassable III".',
-      'Le TP-Kill est autorisé, mais pas les demandes de tp dans le chat.',
-      'Ce message a été écrit par Isotheis.',
-      'Les joueurs sont capables de réfléchir seuls.',
-      //'dem0niak joue sur un compte joueur actuellement, le nom reste secret.',
-      'Le serveur Obsifight possède un Twitter et un Facebook.',
-      'Au départ, il y avait 3 fondateurs: antoinewin, dem0niak et Suertzz.',
-      'L\'usebug bateau a toujours été interdit.',
-      'Le kit "Obsidien" s\'appelait auparavant kit warrior.',
-      'Les KOTH ont fait leur apparition pour la première fois lors du début de la v4.',
-      'dem0niak était fondateur à la v1 mais il avait fait le choix d\'être aussi modérateur.',
-      'Obsifight était à la v1/v2 parmi les meilleurs pvp/faction francophones ; l\'objectif de la v5 est de revenir sur ces bases.',
-      'Un nouveau règlement pour les factions a été mis en place pour v5.',
-      'La TNT AU Xénotium a fait son apparition en version 4.',
-      'Le manganèse a disparu lors de la v3 mais a fait son retour lors de la v4.',
-      'Le xénotium et l\'obsidienne n\'étaient pas présent au début de la v1 mais sont arrivés pendant cette version.',
-      'Le bloc d\'obsidienne casse en 1 coup de tnt.',
-      'Il y a au total 30 messages pour ce "Le saviez-vous ?".'
-    ));
+    // did you know
+    $cache = Cache::read('didYouKnow', 'data');
+    if (!$cache) {
+      $this->didYouKnowModel = ClassRegistry::init('Obsi.DidYouKnow');
+      $didYouKnow = array_map(function ($el) {
+
+        return $el['DidYouKnow']['text']; // return text element
+      }, $this->didYouKnowModel->find('all'));
+      Cache::write('didYouKnow', $didYouKnow, 'data');
+    } else {
+      $didYouKnow = $cache;
+    }
+    $this->controller->set(compact('didYouKnow'));
   }
 
 }
--- SQL/schema.php
+++ SQL/schema.php
@@ -32,6 +32,16 @@ class ObsiAppSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
 	);
 
+  public $obsi__did_you_knows = array(
+    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+    'text' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+    'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+    'indexes' => array(
+      'PRIMARY' => array('column' => 'id', 'unique' => 1)
+    ),
+    'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+  );
+
 	public $obsi__email_update_histories = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
